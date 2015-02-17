8b42c7d08a0f3721e2f2d6227fd2d86785166e70
Thao Le <thaolt@songphi.com>
2015-02-18 05:00:46 +0700

    clean up features from pro version

--- bol/configs.php
+++ bol/configs.php
@@ -1,87 +1,73 @@
 <?php
 
 /**
-* 
-*/
+ *
+ */
 class SPVIDEOLITE_BOL_Configs
 {
-  const PLUGINKEY = 'spvideolite';
-  /**
-   * Default configurations
-   */
-  public $defaults = array(
-    'features.upload_video' => true,
-    'features.categories' => true,
-    'features.importers' => true,
-    'tweaks.desc_show_more' => true,
-    'tweaks.fix_long_titles' => true,
-    'tweaks.correct_player_size' => true,
-    'tweaks.player_enlargable' => true,    
-    'tweaks.link_import' => true,    
-    'processor' => 'Selfservice'
-  );
-
-  protected static $classInstance = null;
-
-  private $configs = null;
-
-  private $changes = array();
-
-  public static function getInstance() {
-    if ( null === self::$classInstance ) {
-      self::$classInstance = new self();
+    const PLUGINKEY = 'spvideolite';
+    
+    /**
+     * Default configurations
+     */
+    public $defaults = array('features.upload_video' => true, 'features.categories' => true, 'features.importers' => true, 'tweaks.desc_show_more' => true, 'tweaks.fix_long_titles' => true, 'tweaks.correct_player_size' => true, 'tweaks.player_enlargable' => true, 'tweaks.link_import' => true, 'processor' => 'Selfservice');
+    
+    protected static $classInstance = null;
+    
+    private $configs = null;
+    
+    private $changes = array();
+    
+    public static function getInstance() {
+        if (null === self::$classInstance) {
+            self::$classInstance = new self();
+        }
+        return self::$classInstance;
     }
-    return self::$classInstance;
-  }
-
-  protected function __construct() {
-    $this->configs = OW::getConfig()->getValues( self::PLUGINKEY );
-    if (!is_array($this->configs)) $this->configs = array();
-    register_shutdown_function(array(&$this,'saveConfigs'));
-  }
-
-  public function get($key) {
-    if (!isset($this->configs[$key])) {
-      if (isset($this->defaults[$key]))
-        $this->configs[$key] = $this->defaults[$key];
-      else
-        throw new Exception("Error Reading Configuration", 1);
+    
+    protected function __construct() {
+        $this->configs = OW::getConfig()->getValues(self::PLUGINKEY);
+        if (!is_array($this->configs)) $this->configs = array();
+        register_shutdown_function(array(&$this, 'saveConfigs'));
     }
-    return $this->configs[$key];
-  }
-
-  public function keyExists($key) {
-    return in_array($key, array_keys($this->getValues()));
-  }
-
-  public function set($key,$value) {
-    if (isset($this->configs[$key]) && $this->configs[$key]==$value) return;
-    $this->changes[] = $key;
-    $this->configs[$key] = $value;
-  }
-
-  public function searchKey($keyPattern) {
-    $matches = array();
-    preg_match_all($keyPattern, implode(PHP_EOL, array_keys($this->getValues())), $matches);
-    if (is_array($matches) && count($matches)>0 && is_array($matches[0]))
-      return $matches[0];
-    else
-      return false;
-  }
-
-  public function getValues() {
-    return array_merge($this->defaults, $this->configs);
-  }
-
-  public function saveConfigs() {
-    if (!count($this->changes)>0) return;
-    foreach ($this->changes as $key) {
-      if (OW::getConfig()->configExists(self::PLUGINKEY,$key)) {
-        OW::getConfig()->saveConfig(self::PLUGINKEY,$key,$this->configs[$key]);
-      } else {
-        OW::getConfig()->addConfig(self::PLUGINKEY,$key,$this->configs[$key]);
-      }  
+    
+    public function get($key) {
+        if (!isset($this->configs[$key])) {
+            if (isset($this->defaults[$key])) $this->configs[$key] = $this->defaults[$key];
+            else throw new Exception("Error Reading Configuration", 1);
+        }
+        return $this->configs[$key];
     }
-
-  }
+    
+    public function keyExists($key) {
+        return in_array($key, array_keys($this->getValues()));
+    }
+    
+    public function set($key, $value) {
+        if (isset($this->configs[$key]) && $this->configs[$key] == $value) return;
+        $this->changes[] = $key;
+        $this->configs[$key] = $value;
+    }
+    
+    public function searchKey($keyPattern) {
+        $matches = array();
+        preg_match_all($keyPattern, implode(PHP_EOL, array_keys($this->getValues())), $matches);
+        if (is_array($matches) && count($matches) > 0 && is_array($matches[0])) return $matches[0];
+        else return false;
+    }
+    
+    public function getValues() {
+        return array_merge($this->defaults, $this->configs);
+    }
+    
+    public function saveConfigs() {
+        if (!count($this->changes) > 0) return;
+        foreach ($this->changes as $key) {
+            if (OW::getConfig()->configExists(self::PLUGINKEY, $key)) {
+                OW::getConfig()->saveConfig(self::PLUGINKEY, $key, $this->configs[$key]);
+            } else {
+                OW::getConfig()->addConfig(self::PLUGINKEY, $key, $this->configs[$key]);
+            }
+        }
+    }
 }
--- init.php
+++ init.php
@@ -4,6 +4,7 @@ define('SPVIDEOLITE_DIR_IMPORTERS', SPVIDEOLITE_DIR_ROOT . DS . 'importers');
 define('SPVIDEOLITE_DIR_USERFILES', OW::getPluginManager()->getPlugin('spvideolite')->getUserFilesDir());
 define('SPVIDEOLITE_DIR_PLUGINFILES', OW::getPluginManager()->getPlugin('spvideolite')->getPluginFilesDir());
 $spvlConfig = SPVIDEOLITE_BOL_Configs::getInstance();
+
 // Routers declaration
 OW::getRouter()->addRoute(new OW_Route('spvideolite.admin', 'admin/plugins/spvideolite', 'SPVIDEOLITE_CTRL_Admin', 'index'));
 
@@ -36,7 +37,8 @@ OW::getRouter()->addRoute(new OW_Route('spvideolite.proxy_args', 'spvideolite/pr
 OW::getRouter()->addRoute(new OW_Route('spvideolite.categories', 'spvideolite/categories', 'SPVIDEOLITE_CTRL_Spvideo', 'categories'));
 
 try {
-     // check if base video plugin is installed and active
+    
+    // check if base video plugin is installed and active
     OW::getPluginManager()->getPlugin('video');
     
     // Initialize helper instance
@@ -46,30 +48,25 @@ try {
         
         // Events handling
         $eventHandler = new SPVIDEOLITE_CLASS_EventHandler();
-
+        
         OW::getEventManager()->bind('core.after_route', array($eventHandler, 'initServiceHooking'));
-
-        if ($spvlConfig->get('tweaks.link_import'))
-            OW::getEventManager()->bind(OW_EventManager::ON_BEFORE_DOCUMENT_RENDER, array($eventHandler, 'replaceVideoAddView'));
-
-        if ($spvlConfig->get('tweaks.desc_show_more'))
-            OW::getEventManager()->bind('video.collect_video_toolbar_items', array($eventHandler, 'showLessVideoDescription'));
-
-        if ($spvlConfig->get('tweaks.correct_player_size'))
-            OW::getEventManager()->bind('video.collect_video_toolbar_items', array($eventHandler, 'correctPlayerSize'));
-
-        if ($spvlConfig->get('tweaks.player_enlargable'))
-            OW::getEventManager()->bind('video.collect_video_toolbar_items', array($eventHandler, 'addLargerPlayerButton'));
-
-        if ($spvlConfig->get('tweaks.fix_long_titles'))
-        OW::getEventManager()->bind(OW_EventManager::ON_BEFORE_DOCUMENT_RENDER, array($eventHandler, 'fixLongTitles'));
+        
+        if ($spvlConfig->get('tweaks.link_import')) OW::getEventManager()->bind(OW_EventManager::ON_BEFORE_DOCUMENT_RENDER, array($eventHandler, 'replaceVideoAddView'));
+        
+        if ($spvlConfig->get('tweaks.desc_show_more')) OW::getEventManager()->bind('video.collect_video_toolbar_items', array($eventHandler, 'showLessVideoDescription'));
+        
+        if ($spvlConfig->get('tweaks.correct_player_size')) OW::getEventManager()->bind('video.collect_video_toolbar_items', array($eventHandler, 'correctPlayerSize'));
+        
+        if ($spvlConfig->get('tweaks.player_enlargable')) OW::getEventManager()->bind('video.collect_video_toolbar_items', array($eventHandler, 'addLargerPlayerButton'));
+        
+        if ($spvlConfig->get('tweaks.fix_long_titles')) OW::getEventManager()->bind(OW_EventManager::ON_BEFORE_DOCUMENT_RENDER, array($eventHandler, 'fixLongTitles'));
     }
     
     // adding package pointers for importers
     OW::getAutoloader()->addPackagePointer('SPVIDEOLITE_IMP', SPVIDEOLITE_DIR_IMPORTERS);
-}
-catch(Exception $err) {
-     // failed to detect base video plugin
+} catch(Exception $err) {
+    
+    // failed to detect base video plugin
     
     
 }
