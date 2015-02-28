ecefa99bf0bd5d21d360b01ce67a9458da2f83da
Thao Le <thaolt@songphi.com>
2015-02-28 12:44:52 +0700

    resolved #225 - added vk.com importer

--- classes/clip_service.php
+++ classes/clip_service.php
@@ -32,10 +32,6 @@ class SPVIDEOLITE_CLASS_ClipService
     }
     
     public function validateClipCode($code, $provider = null) {
-        // replace http/https for solving mixed content warning
-        $code = str_replace("http://", "//", $code);
-        $code = str_replace("https://", "//", $code);
-
         // alternative way to validate embed code
         $iframeTag = "/<iframe.+?<\/iframe>/is";
         $embedTag = "/<embed.+?<\/embed>/is";
---
+++ importers/vk.php
@@ -0,0 +1,109 @@
+<?php
+/**
+* http://vk.com/video294547478_172133378
+* <iframe src="//vk.com/video_ext.php?oid=294547478&id=172133378&hash=4f2e78bb0974cd07&hd=1" width="640" height="360"  frameborder="0"></iframe>
+* 4f2e78bb0974cd07
+* hash2\":\"4f2e78bb0974cd07
+*/
+class SPVIDEOLITE_IMP_Vk implements SPVIDEOLITE_CLASS_IImporter
+{
+	private static $regexp = '#vk\.com.*/video(\d+_\d+)#i';
+	private static $regexpIdIndex = 1;
+	private static $embedTemplate = '<iframe src="//vk.com/video_ext.php?oid={ownerId}&id={videoId}&hash={hash}&hd=1" width="640" height="360"  frameborder="0"></iframe>';
+
+	public static function getRegExp() {
+		return self::$regexp;
+	}
+
+	public static function getRegExpIdentifierIndex() {
+		return self::$regexpIdIndex;
+	}
+
+	public static function embedApplyVideoId($videoId) {
+		$ids = explode('_', $videoId);
+		$oid = $ids[0];
+		$id = $ids[1];
+		$hash = $ids[2];
+		$code = str_replace('{videoId}', $id, self::$embedTemplate);
+		$code = str_replace('{ownerId}', $oid, $code);
+		$code = str_replace('{hash}', $hash, $code);
+		return $code;
+	}
+	
+	public static function getClipIdentifier( $url ) {
+		$matches = array();
+		if (preg_match(self::$regexp, $url, $matches)) {			
+			return $matches[self::$regexpIdIndex];
+		} else {
+			throw new Exception('Unmatched URL of service');
+		}
+	}
+
+	public static function getClipDetailByUrl( $url ) {
+		$id = self::getClipIdentifier( $url );
+		return self::getClipDetailByIdentifier($id);
+	}
+
+	public static function getClipDetailByIdentifier( $id ) {
+		$video = new stdClass;
+		# XML data URL
+		$file_data = 'http://vk.com/video'.$id;
+		$video->html_url = $file_data;
+
+		# XML
+		$html = file_get_contents($file_data);
+
+		$matches = array();
+
+		$jsonRegex = '#var\svars\s=\s(.*?)\\\\n#i';
+		preg_match_all($jsonRegex, $html, $matches);
+		$json = $matches[1][0];
+		$json = stripcslashes($json);
+		if (substr($json, -1) == ';')
+			$json = substr($json, 0, -1);
+		$json = json_decode($json, true);
+
+		# Title
+		$video->title = $json['md_title'] ? strval($json['md_title']) : null;
+		
+		# Description
+		$description_query = '#<div class="video_row_info_description">(.*?)<\/div>#i';
+		$description = array();
+		preg_match_all($description_query, $html, $description);
+		$description = isset($description[1][0])?$description[1][0]:false;
+		$video->description = $description ? strval(trim($description)) : null;
+		
+		# Tags
+		$video->tags = null;
+		
+		# Duration
+		$video->duration = null;
+		
+		# Author & author URL
+		$video->author_url = null;
+		
+		# Publication date
+		$video->date_published = null;
+		
+		# Last update date
+		$video->date_updated = null;
+		
+		# Thumbnails
+		$thumbnail = new stdClass;
+		$thumbnail->url = $json['jpg'];
+		$thumbnail->width = 320;
+		$thumbnail->height = 240;
+		$video->thumbnails[] = $thumbnail;
+		
+		# Player URL
+		$video->player_url = null;
+		
+		# FLV file URL
+		$video->files = array();
+		
+		# Embed Code
+		$video->embedCode = self::embedApplyVideoId($id . '_' . $json['hash2']);
+		return $video;
+	}
+}
+
