501f196d3b6b17939b93f8f256b2b650c33cb4cf
Thao Le <thaolt@songphi.com>
2015-03-20 17:06:51 +0700

    added package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('bower.json'),
+    pkg: grunt.file.readJSON('package.json'),
     copy: {
       main: {
         nonull: true,
