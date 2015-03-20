12a49bf731c5c5245cd55ebd6fe3c6fc130cfa47
Thao Le <thaolt@songphi.com>
2015-03-20 16:51:03 +0700

    bower cfg & grunt task added

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('bower.json'),
+    copy: {
+      main: {
+        nonull: true,
+        files: [
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['*.js'], dest: 'static/js/videojs/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/lang', src: ['*'], dest: 'static/js/videojs/lang/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['*.css'], dest: 'static/css/videojs/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['font/*'], dest: 'static/css/videojs/'},
+        ]
+      }          
+    }         
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  
+  // Default task(s).
+  grunt.registerTask('default', ['copy']);
+
+};
