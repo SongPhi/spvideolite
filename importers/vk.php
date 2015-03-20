1e872e7d16f810ec4b36c13f4341d14d50e95864
Thao Le <thaolt@songphi.com>
2015-03-20 16:01:25 +0700

    applied license all over the place

--- importers/vk.php
+++ importers/vk.php
@@ -1,10 +1,21 @@
 <?php
 /**
-* http://vk.com/video294547478_172133378
-* <iframe src="//vk.com/video_ext.php?oid=294547478&id=172133378&hash=4f2e78bb0974cd07&hd=1" width="640" height="360"  frameborder="0"></iframe>
-* 4f2e78bb0974cd07
-* hash2\":\"4f2e78bb0974cd07
-*/
+ * Copyright 2015 SongPhi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
 class SPVIDEOLITE_IMP_Vk implements SPVIDEOLITE_CLASS_IImporter
 {
 	private static $regexp = '#vk\.com.*/video(\d+_\d+)#i';
