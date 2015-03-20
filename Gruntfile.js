90b803df95dbd97253bf19d6ae9eb17de8931838
Thao Le <thaolt@songphi.com>
2015-03-20 21:43:26 +0700

    clean out all vendor source code

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,15 @@ module.exports = function(grunt) {
       main: {
         nonull: true,
         files: [
-          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['*.js'], dest: 'static/js/videojs/'},
-          {expand: true, cwd: 'bower_components/videojs/dist/video-js/lang', src: ['*'], dest: 'static/js/videojs/lang/'},
-          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['*.css'], dest: 'static/css/videojs/'},
-          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['font/*'], dest: 'static/css/videojs/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['*.js'], dest: 'static/js/vendor/videojs/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/lang', src: ['*'], dest: 'static/js/vendor/videojs/lang/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['*.css'], dest: 'static/css/vendor/videojs/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['font/*'], dest: 'static/css/vendor/videojs/'},
+          {expand: true, cwd: 'bower_components/videojs/dist/video-js/', src: ['font/*'], dest: 'static/css/vendor/videojs/'},
+          {expand: true, cwd: 'bower_components/jquery.easing/js/', src: ['*.min.js'], dest: 'static/js/vendor/'},
+          {expand: true, cwd: 'bower_components/js-base64/', src: ['*.min.js'], dest: 'static/js/vendor/'},
+          {expand: true, cwd: 'bower_components/jquery-toggles/', src: ['*.min.js'], dest: 'static/js/vendor/'},
+          {expand: true, cwd: 'bower_components/jquery-toggles/css/', src: ['*.min.js'], dest: 'static/css/vendor/jquery-toggles/'},
         ]
       }          
     }
