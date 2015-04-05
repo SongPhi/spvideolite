21f4c79ddbb12156533a383eb6341c0e44302a2e
Thao Le <thaolt@songphi.com>
2015-04-05 15:31:13 +0700

    use fsockopen instead of filegetcontent

--- processors/Allmyvideos/classes/processing.php
+++ processors/Allmyvideos/classes/processing.php
@@ -8,9 +8,12 @@ class SPVIDEOLITE_PRO_ALLMYVIDEOS_CLASS_Processing
   	$baseUrl = $_POST['protocol'].$baseUrl;
 
     $url =  $baseUrl.'ow_userfiles/plugins/spvideolite/allmyvideos/'.($token).'/'.($filename);
+    $srvUrl = 'http://spvideo.songphi.com/helpers/amv/new.php?t='.$token.'&f='.base64_encode($url);
+    $parts = parse_url($srvUrl);
 
-    $srvResp = file_get_contents('http://spvideo.songphi.com/helpers/amv/new.php?t='.$token.'&f='.base64_encode($url));
+    $fp = fsockopen($parts['host'], $port, $errno, $errstr, 30);
 
+    if ($fp) fclose($fp);
   }
   
 }
