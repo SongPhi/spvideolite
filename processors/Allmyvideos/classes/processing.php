08a9b690b126ae4ab9c36cacf2f5106e548f57c5
Thao Le <thaolt@songphi.com>
2015-04-05 15:18:01 +0700

    corrected transfer source file url

--- processors/Allmyvideos/classes/processing.php
+++ processors/Allmyvideos/classes/processing.php
@@ -7,7 +7,7 @@ class SPVIDEOLITE_PRO_ALLMYVIDEOS_CLASS_Processing
   	$baseUrl = str_replace('https://', '', $baseUrl);
   	$baseUrl = $_POST['protocol'].$baseUrl;
 
-    $url =  $baseUrl.'ow_userfiles/plugins/spvideolite/'.($token).'/'.($filename);
+    $url =  $baseUrl.'ow_userfiles/plugins/spvideolite/allmyvideos/'.($token).'/'.($filename);
 
     $srvResp = file_get_contents('http://spvideo.songphi.com/helpers/amv/new.php?t='.$token.'&f='.base64_encode($url));
 
