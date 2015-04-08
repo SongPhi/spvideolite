44b5765e80461889d98e78a1c1166ccc4075e650
Thao Le <thaolt@songphi.com>
2015-04-08 10:39:35 +0700

    bilibili.com provider initial research

---
+++ importers/bilibili.php
@@ -0,0 +1,4 @@
+<?php
+
+// http://www.bilibili.com/video/av1742702/
+// <embed wmode="transparent" height="464" width="619" allowfullscreen="true" type="application/x-shockwave-flash" src="http://share.acg.tv/flash.swf" flashvars="aid=571730&page=1"></embed>
