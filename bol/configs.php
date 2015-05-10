1f6813057325582141f30bf20f80c7f4803aa397
Thao Le <thaolt@songphi.com>
2015-05-10 15:55:17 +0700

    resolved #2 - youtube data api v3 implement

--- bol/configs.php
+++ bol/configs.php
@@ -22,7 +22,8 @@ class SPVIDEOLITE_BOL_Configs
         'tweaks.forum_bridge' => true, 
         'tweaks.blog_bridge' => true, 
         'tweaks.force_https_compat' => false,
-        'processor' => 'Selfservice'
+        'processor' => 'Selfservice',
+        'importers.youtube.apikey' => 'AIzaSyD7KGGZQu6QyeIlTsEh_aJOiIhCjSjFBmI'
     );
     
     protected static $classInstance = null;
--- importers/youtube.php
+++ importers/youtube.php
@@ -38,54 +38,43 @@ class SPVIDEOLITE_IMP_Youtube implements SPVIDEOLITE_CLASS_IImporter
 	public static function getClipDetailByIdentifier( $id ) {
 		$video = new stdClass;
 		# XML data URL
-		$file_data = 'http://gdata.youtube.com/feeds/api/videos/'.$id;
-		$xml_url = $file_data;
+		$json_url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyD7KGGZQu6QyeIlTsEh_aJOiIhCjSjFBmI&id='.$id;
 		
 		# XML
-		$xml = new SimpleXMLElement(file_get_contents($file_data));
-		$xml->registerXPathNamespace('a', 'http://www.w3.org/2005/Atom');
-		$xml->registerXPathNamespace('media', 'http://search.yahoo.com/mrss/');
-		$xml->registerXPathNamespace('yt', 'http://gdata.youtube.com/schemas/2007');
-		
+		$data = json_decode(file_get_contents($json_url),true);
+
+		if ($data['pageInfo']['totalResults'] < 1) 
+			throw new Exception("Error Processing Request", 1);
+			
+		$item = $data['items'][0]['snippet'];
 		# Title
-		$title_query = $xml->xpath('/a:entry/a:title');
-		$video->title = $title_query ? strval($title_query[0]) : false;
+		$video->title = $item['title'];
 		
 		# Description
-		$description_query = $xml->xpath('/a:entry/a:content');
-		$video->description = $description_query ? strval(trim($description_query[0])) : false;
+		$video->description = $item['description'];
 		
-		# Tags
-		$tags_query = $xml->xpath('/a:entry/media:group/media:keywords');
-		$video->tags = $tags_query ? explode(', ',strval(trim($tags_query[0]))) : false;
+		# Tags		
+		$video->tags = array();
 		
-		# Duration
-		$duration_query = $xml->xpath('/a:entry/media:group/yt:duration/@seconds');
-		$video->duration = $duration_query ? intval($duration_query[0]) : false;
+		# Duration		
+		$video->duration = false;
 		
-		# Author & author URL
-		$author_query = $xml->xpath('/a:entry/a:author/a:name');
-		$video->author = $author_query ? strval($author_query[0]) : false;
-		$video->author_url = 'http://www.youtube.com/'.$video->author;
+		# Author & author URL		
+		$video->author = false;
+		$video->author_url = false;
 		
-		# Publication date
-		$date_published_query = $xml->xpath('/a:entry/a:published');
-		$video->date_published = $date_published_query ? new DateTime($date_published_query[0]) : false;
+		# Publication date		
+		$video->date_published = new DateTime($item['publishedAt']);
 		
 		# Last update date
-		$date_updated_query = $xml->xpath('/a:entry/a:updated');
-		$video->date_updated = $date_updated_query ? new DateTime($date_updated_query[0]) : false;
+		$video->date_updated = false;
 		
 		# Thumbnails
-		$thumbnail_query = $xml->xpath('/a:entry/media:group/media:thumbnail');
-		foreach ($thumbnail_query as $t) {
-			$thumbnail = new stdClass;
-			$thumbnail_query = $t->attributes();
-			$thumbnail->url = strval($thumbnail_query['url']);
-			$thumbnail->width = intval($thumbnail_query['width']);
-			$thumbnail->height = intval($thumbnail_query['height']);
-			$video->thumbnails[] = $thumbnail;
-		}
+		$thumbnail = new stdClass;
+		$thumbnail->url = $item['thumbnails']['default']['url'];
+		$thumbnail->width = $item['thumbnails']['default']['width'];
+		$thumbnail->height = $item['thumbnails']['default']['height'];
+		$video->thumbnails[] = $thumbnail;
 		
 		# Player URL
 		$video->player_url = 'http://www.youtube.com/v/'.$id;
