e6842dcc5abc6ad680fda64671b443dab7f1d995
Thao Le <thaolt@songphi.com>
2015-05-10 16:07:07 +0700

    fixed conflict with configs.php

--- bol/configs.php
+++ bol/configs.php
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 <?php
 /**
- * Copyright 2015 SongPhi
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
