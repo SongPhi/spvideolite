0a73861d7467e70bd16f659232f4182eb6490a9b
Thao Le <thaolt@songphi.com>
2015-10-03 14:51:10 +0700

    made youtube imported embed code compatible with https

--- importers/youtube.php
+++ importers/youtube.php
@@ -22,7 +22,7 @@ class SPVIDEOLITE_IMP_Youtube implements SPVIDEOLITE_CLASS_IImporter
 {
 	private static $regexp = '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i';
 	private static $regexpIdIndex = 1;
-	private static $embedTemplate = '<iframe width="560" height="315" src="http://www.youtube.com/embed/{videoId}" frameborder="0" allowfullscreen></iframe>';
+	private static $embedTemplate = '<iframe width="560" height="315" src="//www.youtube.com/embed/{videoId}" frameborder="0" allowfullscreen></iframe>';
 
 	public static function getRegExp() {
 		return self::$regexp;
