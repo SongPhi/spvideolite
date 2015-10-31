fb4c46a43cf7b48c6d83721ccb9080ac6ae8a586
Thao Le <thaolt@songphi.com>
2015-10-31 23:26:57 +0700

    upgrade meta data to v2.1.0

--- bol/service.php
+++ bol/service.php
@@ -21,7 +21,7 @@
 class SPVIDEOLITE_BOL_Service
 {
   const PLUGIN_NAME = 'spvideolite';
-  const PLUGIN_VER = 'v2.0.3';
+  const PLUGIN_VER = 'v2.1.0';
   protected static $classInstance = null;
   protected static $processors = null;
 
