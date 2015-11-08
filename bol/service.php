272d82a103f36ab324dc575f002d7a4089bc2416
Thao Le <thaolt@songphi.com>
2015-11-08 17:48:56 +0000

    upgrade version to v2.1.1

--- bol/service.php
+++ bol/service.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Copyright 2015 SongPhi
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,12 +16,12 @@
  */
 
 /**
-* 
+*
 */
 class SPVIDEOLITE_BOL_Service
 {
   const PLUGIN_NAME = 'spvideolite';
-  const PLUGIN_VER = 'v2.1.0';
+  const PLUGIN_VER = 'v2.1.1';
   protected static $classInstance = null;
   protected static $processors = null;
 
